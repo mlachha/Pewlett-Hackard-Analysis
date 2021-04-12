6550c299b3eca31d3f7d24ff02faa9bcc8e736eb
mlachha <47876684+mlachha@users.noreply.github.com>
2021-04-12 14:45:18 -0400

    Add files via upload
    changing the files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,50 @@
+--PART 1, creating the retirement titles table
+SELECT 
+e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
+SELECT * FROM retirement_titles
+
+-- part 2, Use Distinct with ORDER BY to remove duplicate rows 
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no ASC, to_date DESC;
+
+--part 3,creat retiring titles table
+SELECT COUNT (title), 
+title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count desc;
+
+--Deliverable 2, Mentorship eligibility 
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentor_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
---
+++ Queries/queries.sql
@@ -0,0 +1,88 @@
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+ON (dm.emp_no = ce.emp_no);
+
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Skill Drill 7.3.6:
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	dpt.dept_name
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS dpt
+		ON (de.dept_no = dpt.dept_no)
+WHERE dept_name = 'Sales';
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	dpt.dept_name
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS dpt
+		ON (de.dept_no = dpt.dept_no)
+WHERE dept_name IN ('Sales','Development');
